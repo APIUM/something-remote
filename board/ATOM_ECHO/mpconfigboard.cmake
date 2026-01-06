5ca49513c230af9a1f693aaac15c55543c6aa3cd
Algy Tynan <algy@tynan.io>
2026-01-06 22:34:00 +1100

    Fix board config to include MicroPython defaults
    Include sdkconfig.base and sdkconfig.ble from MicroPython port.
    Simplify board-specific overrides.

--- board/ATOM_ECHO/mpconfigboard.cmake
+++ board/ATOM_ECHO/mpconfigboard.cmake
@@ -1,6 +1,8 @@
 set(IDF_TARGET esp32)
 
 set(SDKCONFIG_DEFAULTS
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.base
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
     ${CMAKE_CURRENT_LIST_DIR}/sdkconfig.board
 )
 
